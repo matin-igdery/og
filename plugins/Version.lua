4bfe0034834aea0c6f5c1520049570f64eb59e68
matin-igdery <matin_igdery@yahoo.com>
2016-12-09 18:04:36 +0330

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -2,15 +2,12 @@ do
 
 function run(msg, matches)
 return [[ 
-INFERNALTEAM Robot Ver 4.2
-AntispamBot : @InfernalTG
-website : https://arashinfernal.epage.ir
-Our Channel : @INFERNALTEAMCH
-@INFERNALCHANNEL
-@INFERNALTEAM
+ORGINALGAMING Robot Ver 4.2
+AntispamBot : @OrginalBoT
+website : http://orginal-gaming.ir
+Our Channel : @orginalgaming
 Our code is open source but !
 remember : copy right!
-https://github.com/INFERNALTEAM/InfernalTG
 spical thanx...
 imandaneshi ...
 iwals ...
